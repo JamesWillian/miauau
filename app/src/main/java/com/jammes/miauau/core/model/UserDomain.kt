69069df6022a8ed53c8559d0718cc68c66dfed80
James Willian <j.willian2012@hotmail.com>
2023-06-11 15:06:13 -0300

    Improving the layout of Pet Details

--- app/src/main/java/com/jammes/miauau/core/model/UserDomain.kt
+++ app/src/main/java/com/jammes/miauau/core/model/UserDomain.kt
@@ -3,6 +3,9 @@ package com.jammes.miauau.core.model
 data class UserDomain(
     val uid: String,
     val name: String,
+    val location: String,
+    val about: String,
+    val phone: String,
     val email: String,
     val photoUrl: String
 )
